97bb17dffe4c56ba531fa6aaee7bb4ca6a86d93c
user <eltorxid@gmail.com>
2021-03-12 14:24:28 +0300

    winlab

--- app/src/androidTest/java/braingame/amax/myapplication/BlackBoxTesting/LaunchTest.java
+++ app/src/androidTest/java/braingame/amax/myapplication/BlackBoxTesting/LaunchTest.java
@@ -7,6 +7,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObject2;
 import androidx.test.uiautomator.Until;
 
@@ -21,7 +22,7 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 public class LaunchTest {
 
     @Before
-    public void clearCacheAndData(){
+    public void clearCacheAndData() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             InstrumentationRegistry.getInstrumentation().getUiAutomation()
                     .executeShellCommand("pm clear " + "ru.winelab");
@@ -29,11 +30,42 @@ public class LaunchTest {
     }
 
     @Test
-    public void launchApp(){
+    public void launchApp() throws InterruptedException {
 
-       UiDevice device = UiDevice.getInstance(getInstrumentation());
-       device.wait(Until.hasObject(By.desc("WineLab")), 3000);
-       UiObject2 appsButton = device.findObject(By.desc("WineLab"));
-       appsButton.click();
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        device.wait(Until.hasObject(By.desc("WineLab")), 3000);
+        UiObject2 appsButton = device.findObject(By.desc("WineLab"));
+        appsButton.click();
+
+        device.wait(Until.hasObject(By.desc("ПОДТВЕРДИТЬ И ВОЙТИ")), 7000);
+        UiObject2 btnAcceptAge = device.findObject(By.text("ПОДТВЕРДИТЬ И ВОЙТИ"));
+        btnAcceptAge.click();
+
+        device.wait(Until.hasObject(By.desc("ЗАПРЕТИТЬ")), 2000);
+        UiObject2 btnBlockGeo = device.findObject(By.text("ЗАПРЕТИТЬ"));
+        btnBlockGeo.click();
+
+        device.wait(Until.hasObject(By.desc("ПРОПУСТИТЬ")), 2000);
+        UiObject2 btnPassLocation = device.findObject(By.text("ПРОПУСТИТЬ"));
+        btnPassLocation.click();
+
+        device.wait(Until.hasObject(By.desc("ЗАПРЕТИТЬ")), 2000);
+        UiObject2 btnBlockGeo2 = device.findObject(By.text("ЗАПРЕТИТЬ"));
+        btnBlockGeo2.click();
+
+        device.wait(Until.hasObject(By.desc("ПРОПУСТИТЬ")), 2000);
+        UiObject2 btnPassLocation2 = device.findObject(By.text("ПРОПУСТИТЬ"));
+        btnPassLocation2.click();
+
+        device.wait(Until.hasObject(By.desc("Продолжить как гость")), 2000);
+        UiObject2 btnСontinueAsGuest = device.findObject(By.text("Продолжить как гость"));
+        btnСontinueAsGuest.click();
     }
+
+//    @Test
+//    public void acceptAge() throws InterruptedException {
+//
+//        UiDevice device = UiDevice.getInstance(getInstrumentation());
+//
+//    }
 }
